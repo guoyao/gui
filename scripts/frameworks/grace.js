d37de0a6e99dff813c9fce1390faeeb6c4c68060
guoyao <guoyao.dev@gmail.com>
2013-07-29 15:12:22 +0800

    add vertical dropdown navigation component

--- scripts/frameworks/grace.js
+++ scripts/frameworks/grace.js
@@ -6,20 +6,39 @@
 
 (function (document, $) {
 
-    var isIe6 = $.browser.msie && $.browser.version == "6.0";
+    var browserInfo = {
+        isIE: $.browser.msie,
+        version: Number($.browser.version),
+        documentMode: Number(document.documentMode)
+    };
 
     $.fn.graceNav = function () {
-        if (isIe6) {
-            this.find("> li").css("float", "left");
-            return this.find("li").hover(function () {
-                $(this).find("> ul").css("display", "block");
-            }, function () {
-                $(this).find("> ul").css("display", "none");
-            });
+        var isVertical = this.hasClass("grace-nav-vertical");
+        if (browserInfo.isIE) {
+            if (isVertical && (browserInfo.version <= 7 || browserInfo.documentMode <= 7)) {
+                this.find("> li").hover(function () {
+                    if($(this).find("> ul").length > 0) {
+                        $(this).css("margin-bottom", "-3px");
+                    }
+                }, function () {
+                    $(this).css("margin-bottom", 0);
+                })
+            }
+            if(browserInfo.version <= 6) {
+                if(isVertical) {
+                    this.find("> li").css("width", "120px").css("position", "relative");
+                }
+                else {
+                    this.find("> li").css("float", "left");
+                }
+                this.find("li").hover(function () {
+                    $(this).find("> ul").css("display", "block");
+                }, function () {
+                    $(this).find("> ul").css("display", "none");
+                });
+            }
         }
-        else {
-            return this;
-        }
+        return this;
     };
 
 })(document, jQuery);
