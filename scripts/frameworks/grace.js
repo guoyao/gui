f22f02d0cecf4a516d22bbad20735df3b7bf402c
guoyao <guoyao.dev@gmail.com>
2013-07-29 17:40:52 +0800

    update dropdown navigation component

--- scripts/frameworks/grace.js
+++ scripts/frameworks/grace.js
@@ -13,32 +13,34 @@
     };
 
     $.fn.graceNav = function () {
-        var isVertical = this.hasClass("grace-nav-vertical");
-        if (browserInfo.isIE) {
-            if (isVertical && (browserInfo.version <= 7 || browserInfo.documentMode <= 7)) {
-                this.find("> li").hover(function () {
-                    if($(this).find("> ul").length > 0) {
-                        $(this).css("margin-bottom", "-3px");
+        return this.each(function () {
+            var $graceNav = $(this);
+            var isVertical = $graceNav.hasClass("grace-nav-vertical");
+            if (browserInfo.isIE) {
+                if (isVertical && (browserInfo.version <= 7 || browserInfo.documentMode <= 7)) {
+                    $graceNav.find("> li").hover(function () {
+                        if($(this).find("> ul").length > 0) {
+                            $(this).css("margin-bottom", "-3px");
+                        }
+                    }, function () {
+                        $(this).css("margin-bottom", 0);
+                    })
+                }
+                if(browserInfo.version <= 6) {
+                    if(isVertical) {
+                        $graceNav.find("> li").css("width", "120px");
                     }
-                }, function () {
-                    $(this).css("margin-bottom", 0);
-                })
-            }
-            if(browserInfo.version <= 6) {
-                if(isVertical) {
-                    this.find("> li").css("width", "120px").css("position", "relative");
+                    else {
+                        $graceNav.find("> li").css("float", "left");
+                    }
+                    $graceNav.find("li").hover(function () {
+                        $(this).find("> ul").css("display", "block");
+                    }, function () {
+                        $(this).find("> ul").css("display", "none");
+                    });
                 }
-                else {
-                    this.find("> li").css("float", "left");
-                }
-                this.find("li").hover(function () {
-                    $(this).find("> ul").css("display", "block");
-                }, function () {
-                    $(this).find("> ul").css("display", "none");
-                });
             }
-        }
-        return this;
+        });
     };
 
 })(document, jQuery);
