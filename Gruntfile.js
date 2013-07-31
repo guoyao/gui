f8a99019b33820b6d7c6086afec8a934426ce188
guoyao <guoyao.dev@gmail.com>
2013-07-31 18:39:42 +0800

    add grunt support

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+/**
+ * User: guoyao
+ * Time: 07-31-2013 18:03
+ * Blog: http://www.guoyao.me
+ */
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
