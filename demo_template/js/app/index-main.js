d644d025ee8ff07bae61d8c6269958c499d86383
alfredzh <alfredzh2@gmail.com>
2013-09-22 18:37:29 +0800

    Added demo of tooltip!

--- demo_template/js/app/index-main.js
+++ demo_template/js/app/index-main.js
@@ -17,7 +17,8 @@ define(["./demo"], function (demo) {
         new Module("Date Picker", "date-picker"),
         new Module("Button", "button"),
         new Module("Button Bar", "button-bar"),
-        new Module("Carousel", "carousel")
+        new Module("Carousel", "carousel"),
+        new Module("Tooltip", "tooltip")
     ], {
         modulesDirectory: "modules/components"
     }).init();
---
+++ demo_template/modules/components/tooltip/_index.js
@@ -0,0 +1,4 @@
+require(["jquery", "prettify", "gui"], function ($, prettify) {
+    $(".tooltip-demo").guiTooltip();
+    prettify.prettyPrint();
+});
