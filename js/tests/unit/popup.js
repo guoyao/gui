7972953412b8141e7cfb3f66aec19be295dfb911
alfredzh <alfredzh2@gmail.com>
2013-09-22 16:09:23 +0800

    Added ie support style for popup module!

--- js/tests/unit/popup.js
+++ js/tests/unit/popup.js
@@ -1,11 +1,28 @@
-/**
- * Author: guoyao
- * Time: 08-27-2013 10:14
- * Blog: http://www.guoyao.me
- */
+$(function (undefined) {
 
-$(function () {
+	var $ = window.jQuery;
 
-    module("popup");
+	var moduleDebug = $(document).guiPopup('debug');
+
+	//get the test wrapper
+	var mainWp = $("#qunit-fixture");
+
+	//
+	module("tooltip", {
+		setup: function () {
+			testobj.appendTo(mainWp);
+			//console.log(testobj.html())
+
+			//set local obj
+			moduleDebug.obj = $(".tooltip-demo");
+		}, teardown: function () {
+
+		}
+	});
+
+	//
+	test("initital append popup node",function(){
+		
+	})
 
 });
