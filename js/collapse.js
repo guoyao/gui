13c8d532576b3994e8493f3960c4cb4c87b9909d
guoyao <guoyao.dev@gmail.com>
2013-10-11 16:47:07 +0800

    remove unused code

--- js/collapse.js
+++ js/collapse.js
@@ -32,87 +32,7 @@
 		$ = window.jQuery,
 		gui = window.gui,
 		old = $.fn.guiCollapse;
-/*
-<<<<<<< HEAD
-	var module = function(obj,option){
-		this._init(obj,option);
-	}
 
-	module.prototype = {
-		_init: function (obj, option) {
-			this.obj = obj;
-			this._initOptions(option);
-			this._recordEleHeight();
-			this._eventHandler();
-		},
-		orgHeight: [],
-		//
-		_initOptions: function (option) {
-			this.defaults = $.extend({}, $.fn.guiCollapse.defaults, option);
-		},
-		_recordEleHeight: function () {
-			var mo = this;
-			$(this.obj)
-				.find('.' + this.defaults.switchTabClass)
-				.each(function (i) {
-					if (!$(this).is(":visible")) {
-						$(this).show();
-						mo.orgHeight[i] = $(this).height();
-						$(this).hide();
-					} else {
-						mo.orgHeight[i] = $(this).height();
-					}
-				});
-			return mo.orgHeight;
-		},
-		_eventHandler: function () {
-			var mo = this;
-			$(this.obj)
-				.find('.' + this.defaults.switchBtnClass)
-				.each(function (i) {
-					$(this).click(function (e) {
-						//var tabIndex = $(e.target).eq(i);
-
-						var curHeight = $(this).siblings('.' + mo.defaults.switchTabClass).height();
-						var tabContent = $(this).siblings('.' + mo.defaults.switchTabClass);
-
-						var animspeed = mo.defaults.animationDuration;
-						var toggleClass = mo.defaults.switchBtnToggleClass;
-
-						if (curHeight <= 0 || tabContent.is(":hidden")) {
-							tabContent
-								.css({display: "block", height: 0, opacity: 0})
-								.animate({'height': mo.orgHeight[i], 'opacity': 1}, animspeed);
-							$(this).removeClass(toggleClass);
-						} else {
-							tabContent
-								.animate({'height': 0, 'opacity': 0}, animspeed);
-							$(this).addClass(toggleClass);
-						}
-					});
-				});
-		}
-	}
-
-	$.fn.guiCollapse = function (option) {
-
-		return this.each(function () {
-			new module(this,option);
-			//module._init(this, option);
-		});
-	}
-
-	$.fn.guiCollapse.defaults = {
-		switchBtnClass: 'tab-btn',
-		switchBtnToggleClass: 'tab-btn-closed',
-		switchTabClass: 'tab-content',
-		animationDuration: 500
-	};
-
-	//for debug
-	$.fn.guiCollapse.Constructor = module;
-=======
-*/
     // COLLAPSE PUBLIC CLASS DEFINITION
     // ================================
 
@@ -206,7 +126,6 @@
 
     // COLLAPSE NO CONFLICT
     // ====================
-//>>>>>>> 8aa611db3915f048d66ffccb306b541812b2d917
 
 	$.fn.guiCollapse.noConflict = function () {
 		$.fn.guiCollapse = old;
